51850dc27278493113e627bafeb9e9bf1b44f60c
Andy Rozman <andy.rozman@gmail.com>
2010-06-20 18:28:22 +0000

    0.5.1

---
+++ java/src/com/atech/utils/ATechToolsVersion.java
@@ -0,0 +1,8 @@
+package com.atech.utils;
+
+public class ATechToolsVersion
+{
+    public static final String version_displayable = "0.5.1";
+    
+    public static final int version_numeric = 6;
+}
