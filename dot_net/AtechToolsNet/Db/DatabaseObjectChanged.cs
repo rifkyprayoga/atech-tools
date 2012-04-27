0d51a563d425be42b548ca5e886bfdc65db95c55
Andy Rozman <andy.rozman@gmail.com>
2012-04-27 21:43:45 +0000

    1.0.6 changes on 27.4

---
+++ dot_net/AtechToolsNet/Db/DatabaseObjectChanged.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DatabaseObjectChanged.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace AtechTools.Core.Db
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public interface DatabaseObjectChanged
+    {
+
+        bool Changed
+        { get; set; }
+
+    }
+}
